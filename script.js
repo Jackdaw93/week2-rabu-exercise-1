ec3001e646dd92cf01823c66a1e18154195ed334
Rohmad <rohmad37@ymail.com>
2020-06-17 10:53:27 +0700

    Exercise Rabu 1

---
+++ script.js
@@ -0,0 +1,22 @@
+function myName() {
+  let name1 = document.getElementById("name").innerHTML;
+  let phone1 = document.getElementById("phone").innerHTML;
+  let email1 = document.getElementById("email").innerHTML;
+  let pass1 = document.getElementById("password").innerHTML;
+}
+
+function showData() {
+  let name1 = document.getElementById("name").value;
+  let phone1 = document.getElementById("name").value;
+  let email1 = document.getElementById("name").value;
+  let pass1 = document.getElementById("name").value;
+  alert(`
+  Nama Saya: ${name1} \n 
+  No Hp: ${phone1} \n
+  Email: ${email1} \n
+  Password: ${pass1}
+  `);
+}
+
+const button = document.getElementById("show");
+button.addEventListener("click", showData);
